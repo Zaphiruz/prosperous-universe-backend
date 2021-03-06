7794f414eed64958b84cce5faba00de3172d61f3
Zaphiruz <Jacobthomas428@gmail.com>
2021-03-06 11:32:04 -0800

    add https

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
 import { graphqlHTTP } from 'express-graphql';
 import graphqlSchema from './schemas/index';
 import graphqlPlayground from 'graphql-playground-middleware-express';
@@ -38,5 +41,24 @@ app.listen(port, function() {
 		useFindAndModify: false
 	});
 });
+
+let httpServer = http.createServer(app);
+httpServer.listen(80, (err) => {
+	if (err) return console.error(err);
+	console.log('listening on 80');
+})
+
+if (process.env.NODE_ENV === 'production') {
+	const key = fs.readFileSync('/etc/letsencrypt/live/prosperon.app/privkey.pem', 'utf-8');
+	const cert = fs.readFileSync('/etc/letsencrypt/live/prosperon.app/cert.pem');
+	const ca = fs.readFileSync('/etc/letsencrypt/live/prosperon.app/chain.pem');
+
+	let httpServer = https.createServer({key, cert, ca}, app);
+	httpServer.listen(443, (err) => {
+		if (err) return console.error(err);
+		console.log('listening on 443');
+	})
+}
+
 
 mongoose.connection.on('error', console.error.bind(undefined, "Failled to connect to DB!"))
